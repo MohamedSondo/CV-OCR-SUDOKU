cddff396e85726949f3fdc0966c67c0af0dc269f
Monaf Corporation <epsilon@Monafs-MacBook-Pro.local>
2017-12-11 00:56:33 -0500

    first commit

---
+++ SudokuSolver/app/src/main/java/com/example/ComputerVisionFinalProject/sudokusolver/GridRecognizer.java
@@ -0,0 +1,31 @@
+/* CCNY FALL 2017  Computer Vision Final Project
+Mohamed Sondo
+&
+Zhirayr Abrahamyan*/
+package com.example.ComputerVisionFinalProject.sudokusolver;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class GridRecognizer {
+
+    public GridRecognizer() {
+
+    }
+
+    public Mat HoughLines(Mat image) {
+        Mat grayMat = new Mat();
+        Mat cannyEdges = new Mat();
+        Mat lines = new Mat();
+
+        Imgproc.cvtColor(image, grayMat, Imgproc.COLOR_RGB2GRAY);
+
+        Imgproc.Canny(grayMat, cannyEdges, 10, 100);
+
+        Imgproc.HoughLinesP(cannyEdges, lines, 1, Math.PI/180, 50,20,20);
+
+        return lines;
+
+    }
+}
